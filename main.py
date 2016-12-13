cc11305c2f0a0c90c1fa513890b675945f8fec39
Hekku2 <hekku2@gmail.com>
2016-12-13 19:07:36 +0000

    Configuration reading and measurement sending

--- main.py
+++ main.py
@@ -3,8 +3,42 @@
 #os.system('modprobe w1-therm')
 
 from w1thermsensor import W1ThermSensor
+import requests
+import datetime
+import ConfigParser
 
-print("Printing")
+def ConfigSectionMap(config, section):
+    dict1 = {}
+    options = config.options(section)
+    for option in options:
+        try:
+            dict1[option] = config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
+print("Reading config...")
+config = ConfigParser.ConfigParser()
+config.read('config.ini')
+
+login = {
+    'identifier': ConfigSectionMap(config, 'Server')['username'],
+    'password': ConfigSectionMap(config, 'Server')['password']
+}
+r = requests.post(ConfigSectionMap(config, 'Server')['host'] + '/login', data = login)
+token = r.json()['token']
 for sensor in W1ThermSensor.get_available_sensors():
+    id = ConfigSectionMap(config, 'Sensors')[sensor.id]
+    payload = {
+        'token': token, 
+        'timestamp': datetime.datetime.now().isoformat(),
+        'sensor': id,
+        'value': sensor.get_temperature()
+    }
+    result = requests.post(ConfigSectionMap(config, 'Server')['host'] + '/measurement', data = payload)
+    print(result)
     print("Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature()))
-print("Printing ended")
+print("Reading ended")
